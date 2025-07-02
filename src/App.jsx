ad78cd29e732084433eea50d01a653264a3601e4
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-02 16:26:49 -0500

    add delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,33 @@ import CreateArea from './components/CreateArea';
 import Note from './components/Note';
 
 function App(){
+  const [notes, setNotes] = useState([]);
 
+  function addNote(newNote){
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
 
+  function deleteNote(id){
+    setNotes((prevValue) =>{
+      return [...prevValue.filter((note, index) =>
+        index !== id)];
+    });
+  }
   
   return (
     <div className='App'>
       <Header/>
-      <CreateArea/>
-      <Note/>
+      <CreateArea onAdd={addNote}/>
+      {notes.map((note, index) => (
+        <Note key={index} 
+              id={index} 
+              title={note.title} 
+              content={note.content}
+              onDelete={deleteNote}
+        />
+      ))}
     </div>
     
   );
