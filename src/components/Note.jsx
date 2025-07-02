d356714e48e380151e6b8843a6219da20f43ccda
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-02 17:12:09 -0500

    add del svg

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
 
 function Note({title,content, onDelete, id}){
     return(
         <div className="note">
             <h1>{title}</h1>
             <p>{content}</p>
-            <button onClick={() => onDelete(id)}>Delete</button>
+            <button onClick={() => onDelete(id)}><AiOutlineDelete size={28}/></button>
         </div>
     )
 }
