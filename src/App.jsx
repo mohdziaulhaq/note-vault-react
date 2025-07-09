ef5314879c9e5811a9f8a0b9ca252e282d17701f
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-08 23:11:14 -0500

    add Routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,76 @@
-import React, {useState} from 'react';
-import Header from './components/Header';
-import './App.css';
-import CreateArea from './components/CreateArea';
-import Note from './components/Note';
+// import React, {useState} from 'react';
+// import Header from './components/Header';
+// import './App.css';
+// import CreateArea from './components/CreateArea';
+// import Note from './components/Note';
 
-function App(){
-  const [notes, setNotes] = useState([]);
+// function App(){
+//   const [notes, setNotes] = useState([]);
 
-  function addNote(newNote){
-    setNotes((prevValue) => {
-      return [...prevValue, newNote];
-    });
-  }
+//   function addNote(newNote){
+//     setNotes((prevValue) => {
+//       return [...prevValue, newNote];
+//     });
+//   }
 
-  function deleteNote(id){
-    setNotes((prevValue) =>{
-      return [...prevValue.filter((note, index) =>
-        index !== id)];
-    });
-  }
+//   function deleteNote(id){
+//     setNotes((prevValue) =>{
+//       return [...prevValue.filter((note, index) =>
+//         index !== id)];
+//     });
+//   }
   
-  return (
-    <div className='App'>
-      <Header/>
-      <CreateArea onAdd={addNote}/>
-      {notes.map((note, index) => (
-        <Note key={index} 
-              id={index} 
-              title={note.title} 
-              content={note.content}
-              onDelete={deleteNote}
-        />
-      ))}
-    </div>
+//   return (
+//     <div className='App'>
+//       <Header/>
+//       <CreateArea onAdd={addNote}/>
+//       {notes.map((note, index) => (
+//         <Note key={index} 
+//               id={index} 
+//               title={note.title} 
+//               content={note.content}
+//               onDelete={deleteNote}
+//         />
+//       ))}
+//     </div>
     
+//   );
+// };
+// export default App;
+
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Notes from "./pages/Notes";
+import CreateNote from "./pages/CreateNote";
+import Profile from "./pages/Profile";
+import AdminUsers from "./pages/AdminUsers";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/create-note" element={<CreateNote />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 };
+
 export default App;
+
