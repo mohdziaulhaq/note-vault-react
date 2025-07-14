025bec55fbfbdb58fb87aea375147ea6bd205ad4
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-14 06:04:47 -0500

    add new create note

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,6 +1,6 @@
 import api from "../../services/api";
 import React, { useEffect, useState } from "react";
-
+import {Link} from "react-router-dom";
 const Notes = () => {
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -24,8 +24,24 @@ const Notes = () => {
       <h2 className="text-2xl font-semibold mb-4">My Notes</h2>
       {loading ? (
         <p>Loading...</p>
+        
       ) : notes.length === 0 ? (
-        <p>No notes found.</p>
+        <div className="text-center">
+                  <h2 className="text-2xl font-bold text-gray-800 mb-4">
+                    You didn't create any note yet
+                  </h2>
+                  <p className="text-gray-600 mb-6">
+                    Start by creating a new note to keep track of your thoughts.
+                  </p>
+                  <div className="w-full flex justify-center">
+                    <Link to="/create-note">
+                      <button className="flex items-center px-4 py-2 bg-btnColor text-white rounded  focus:outline-none focus:ring-2 focus:ring-blue-300">
+                        {/* <FiFilePlus className="mr-2" size={24} /> */}
+                        Create New Note
+                      </button>
+                    </Link>
+                  </div>
+                </div>
       ) : (
         <div className="grid gap-4 sm:grid-cols-2">
           {notes.map((note) => {
