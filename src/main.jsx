89c59063d11e09d9797c8e8b941efa36a52d7a28
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-16 05:33:29 -0500

    remove browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
