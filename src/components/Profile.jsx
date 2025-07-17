a4f7c767ac622db11727bf1fb47714e9e9f2115e
mohdziaulhaq <mohdziaulhaq123@gmail.com>
2025-07-16 20:50:15 -0500

    add Profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+
+function Profile(){
+    return(
+        <div className="note">
+            Profile
+        </div>
+    )
+}
+
+export default Profile;
